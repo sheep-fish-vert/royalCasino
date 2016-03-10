d6942d9e69af0910cb708f9fbf42cb1286d69e23
Dmitry <dmitry1939@gmail.com>
2016-03-10 15:37:35 +0200

    footer done

--- src/js/partials/develop_1.js
+++ src/js/partials/develop_1.js
@@ -1,6 +1,50 @@
 try{
 
+    function footerAdaptation(){
+
+        function listWrapHeight(){
+
+            $('.footer-top').removeAttr('style');
+
+            if($(window).width()<=768 && $(window).width()>640){
+
+                var arr = [];
+
+                $('.footer-list-wrap').each(function(){
+
+                    arr.push($(this).outerHeight());
+
+                });
+
+                var maxHeight = Math.max.apply(Math, arr);
+
+                $('.footer-top').css({'height':maxHeight+'px'});
+            }
+            else if($(window).width()<=640){
+                $('.footer-top').height($('.footer-top').height()/1.5);
+            }
+
+        }
+
+        listWrapHeight();
+
+        $(window).load(function(){
+
+            listWrapHeight();
+
+        });
+
+        $(window).resize(function(){
+
+            listWrapHeight();
+
+        });
+
+    }
+
     $(document).ready(function(){
+
+        footerAdaptation();
 
     });
 
