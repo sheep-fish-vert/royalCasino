3d7724e3659f394e4f9fa0c1799e1270af227d3e
Dmitry <dmitry1939@gmail.com>
2016-03-11 15:51:32 +0200

    statistic list

--- src/js/partials/develop_1.js
+++ src/js/partials/develop_1.js
@@ -50,7 +50,7 @@ try{
 
     function circleDraw(){
 
-        $('.statistic-content-svg').each(function() {
+        $('.statistic-content-svg').each(function(index, el) {
 
             var svg = $(this).find('svg');
 
@@ -64,6 +64,9 @@ try{
 
             var greenLength = (greenValue * greenFerence)/maxValue;
             var blueLength = (blueValue * blueFerence)/maxValue;
+
+            svg.find('.green-text').text(parseFloat(greenValue).toPrecision(3));
+            svg.find('.blue-text').text(parseFloat(blueValue).toPrecision(3));
 
             svg.find('.green-value').css({'stroke-dasharray':greenLength+' '+greenFerence});
             svg.find('.blue-value').css({'stroke-dasharray':blueLength+' '+blueFerence});
