3de61e7f408ffa697d5af89a3a7b46ad6662c840
Art Garm <artem.garmatyk@gmail.com>
2016-03-11 11:20:08 +0200

    package

--- src/js/partials/develop_2.js
+++ src/js/partials/develop_2.js
@@ -1,30 +1,8 @@
 try{
 
-    function someAvesomeFix(){
-        var ter = Math.max( $('.super-stat>.conteiner').height(),
-            $('.second-block-wrap>.conteiner').height(),
-            $('.slice1>.conteiner').height() + $('.slice2>.conteiner').height() + $('.slice3>.conteiner').height()+4
-        );
-
-        //var ter = $('.super-stat').height();
-        var lastSlice = ter - $('.slice1').height() - $('.slice2').height()-4;
-
-        $('.casino-list-wrap').height(ter);
-        $('.second-block-wrap>.conteiner').height(ter/2-1);
-        $('.slice3').height(lastSlice);
-    }
-
     $(document).ready(function(){
-        /*
-        if ($(window).width()>1024) {
-            someAvesomeFix();
-        }
-        else{
-            $('.casino-list-wrap').css('height', 'auto');
-            $('.second-block-wrap>.conteiner').css('height', 'auto');
-            $('.slice3').css('height', 'auto');
-        }
-        */
+
+
 
     });
 
@@ -33,16 +11,6 @@ try{
     });
 
     $(window).resize(function(){
-        /*
-        if ($(window).width()>1024) {
-            someAvesomeFix();
-        }
-        else{
-            $('.casino-list-wrap').css('height', 'auto');
-            $('.second-block-wrap>.conteiner').css('height', 'auto');
-            $('.slice3').css('height', 'auto');
-        }
-        */
 
     });
 
